a78437fd6c6d76abe8b1215c20fc320a4ba93532
Piyush Datta <piyush.datta@mail.utoronto.ca>
2017-05-29 19:34:10 -0400

    Day 7. Hamming Distance. Really easy O(1) solution using the java method bitCount(). Although, I still need to work on my bit manipulation algorithms.

---
+++ JavaCode365Days/src/Day7_HammingDistance.java
@@ -0,0 +1,23 @@
+
+// Day 6 - Hamming Distance (find the points on the  
+// binary representations of the numbers that are different) 
+// May 29th, 2017
+// Leetcode.com
+// Under algorithms section
+// Big O: O(1)
+
+public class Day7_HammingDistance {
+
+	public int hammingDistance(int x, int y) {
+		// XOR
+		// O(1)
+        return Integer.bitCount(x^y);
+    }
+
+	public static void main(String[] args) {
+		Day7_HammingDistance c = new Day7_HammingDistance();
+		int int1 = 4;
+		int int2 = 1;
+		System.out.println(c.hammingDistance(int1, int2));
+	}
+}
