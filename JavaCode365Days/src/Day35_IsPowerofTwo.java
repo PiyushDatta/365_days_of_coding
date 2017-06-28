03dab10e5ea9344f0a7afb18af8204d3d56a1a6b
Piyush Datta <piyush.datta@mail.utoronto.ca>
2017-06-27 22:33:05 -0400

    Day 35. Sloppy answer, could have worked out a better solution, but nonetheless works well. The sloppy part is the <= .0001. That was a bit of a strecth to meet some edge cases of numbers like 536870912. Found some neat solutions on the forums using bit manipulation though.

---
+++ JavaCode365Days/src/Day35_IsPowerofTwo.java
@@ -0,0 +1,21 @@
+
+// Day 35 - Return true if the number is a power of two
+// June 26th, 2017
+// Leetcode.com
+// Under algorithms section
+// Big O: O(n), 3ms Solution
+
+public class Day35_IsPowerofTwo {
+	
+	public boolean isPowerofTwo(int n) {
+	    return ((Math.log(n) / Math.log(2)) % 1 <= 0.000000001);
+	}
+
+
+    public static void main(String[] args) {
+        Day35_IsPowerofTwo c = new Day35_IsPowerofTwo();
+        int s1 = 32;
+        System.out.println(c.isPowerofTwo(s1));
+
+    }
+}
